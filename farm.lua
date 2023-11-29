9c665d11f6cf9cf946f764dcde61bcfbb3cf134e
FernPickler <60300936+PaluMeoi@users.noreply.github.com>
2023-11-29 03:53:05 -0800

    Farm script: Add movement functions, refueling, and action based on block names
    Added `goRight()` and `goLeft()` functions to enable the turtle to move in those directions. Also added `refuel()` function to check for blocks in front and refuel accordingly. Implemented `takeAction()` function to perform specific actions based on the block name. The script now moves in a more strategic manner, taking action based on the blocks it encounters.

--- farm.lua
+++ farm.lua
@@ -1,17 +1,49 @@
+-- Created by Palurien Meoi
+
+-- Read inventory to set configs
+local left = turtle.getItemDetail(14).name
+local right = turtle.getItemDetail(15).name
+local reset = turtle.getItemDetail(16).name
+
+
 local function move()
     turtle.forward()
     turtle.placeDown()
 end
 
-local function turnAround()
+local function goRight()
     turtle.turnRight()
     move()
-    turtle.turnRight()
 end
+
+local function goLeft()
+    turtle.turnLeft()
+    move()
+end
+
+local function refuel()
+--     Check there's a block in front
+    local status, block = turtle.inspect()
+    turtle.suckUp()
+    turtle.refuel()
+    takeAction(block.name)
+end
+
+local takeAction(blockName)
+    if blockName == left then
+        goLeft()
+    elseif blockName == right then
+        goRight()
+    elseif blockName == reset then
+        refuel()
+    else:
+        move()
+    end
 
 while true do
     if turtle.detect() then
-        turnAround()
+        status, block = turtle.inspectUp()
+        takeAction(block.name)
     else
         move()
     end
